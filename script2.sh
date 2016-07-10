a01374ae22868e9161cbaec14c21f3b797078be0
tharkinvankayala93 <vtharkin.vt@gmail.com>
2016-07-10 13:15:14 -0500

    first commit

---
+++ script2.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+variable1=$1
+variable2=$2
+sumtotal=`expr $variable1 + $variable2`
+echo " the sum is : $sumtotal"
+string="hai tharkin"
+echo "my first statement is $string"
+#echo "give a input value"
+#read myvar
+#echo "the input value is $myvar"
+echo "my numbers $@"
+
+
+
+
